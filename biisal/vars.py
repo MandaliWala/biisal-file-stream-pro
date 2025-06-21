22c45933d84251910bd0890b6028f74d9d6d8673
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-06-21 09:53:43 +0530

    try to fix

--- biisal/vars.py
+++ biisal/vars.py
@@ -24,7 +24,7 @@ class Var(object):
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "6302921275 5019668523").split()]
-    NO_PORT = bool(getenv('NO_PORT', False))
+    NO_PORT = bool(getenv('NO_PORT', True))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Spider_Man_02'))
     if 'DYNO' in environ:
